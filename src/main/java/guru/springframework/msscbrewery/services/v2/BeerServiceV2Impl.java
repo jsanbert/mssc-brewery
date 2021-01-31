f38ae11085cfc438d3c39a6301a578977a140ad5
Jesús Sánchez Bertolín <jsbertolin@gmail.com>
2021-01-31 17:00:32 +0100

    add v2 api

---
+++ src/main/java/guru/springframework/msscbrewery/services/v2/BeerServiceV2Impl.java
@@ -0,0 +1,37 @@
+package guru.springframework.msscbrewery.services.v2;
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import guru.springframework.msscbrewery.web.model.v2.BeerStyleEnum;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceV2Impl implements BeerServiceV2 {
+    @Override
+    public BeerDtoV2 getBeerById(UUID beerId) {
+        return BeerDtoV2.builder().id(UUID.randomUUID())
+                .beerName("Galaxy Cat")
+                .beerStyle(BeerStyleEnum.ALE)
+                .build();
+    }
+
+    @Override
+    public BeerDtoV2 saveBeer(BeerDtoV2 beerDto) {
+        return BeerDtoV2.builder()
+                .id(UUID.randomUUID())
+                .beerName(beerDto.getBeerName())
+                .beerStyle(beerDto.getBeerStyle())
+                .upc(beerDto.getUpc())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID BeerId, BeerDtoV2 beerDto) {
+
+    }
+
+    @Override
+    public void deleteBeer(UUID beerId) {
+
+    }
+}
---
+++ src/main/java/guru/springframework/msscbrewery/web/model/v2/BeerDtoV2.java
@@ -0,0 +1,25 @@
+package guru.springframework.msscbrewery.web.model.v2;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.util.UUID;
+
+/**
+ * Created by jt on 2019-04-20.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDtoV2 {
+    private UUID id;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+}
