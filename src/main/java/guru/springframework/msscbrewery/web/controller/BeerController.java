14f7e14abeef1eacbae33976d95419600568bdbd
Jesús Sánchez Bertolín <jsbertolin@gmail.com>
2021-01-31 17:00:55 +0100

    add validations

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 /**
@@ -30,7 +31,7 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
         BeerDto savedBeer = beerService.saveBeer(beerDto);
 
         HttpHeaders headers = new HttpHeaders();
@@ -41,7 +42,7 @@ public class BeerController {
 
     @PutMapping({"/{beerId}"})
     public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId,
-                                         @RequestBody BeerDto beerDto){
+                                     @Valid @RequestBody BeerDto beerDto){
         beerService.updateBeer(beerId, beerDto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
