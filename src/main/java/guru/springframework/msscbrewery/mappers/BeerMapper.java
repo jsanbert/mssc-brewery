1442a29edd381231e5cb27c29eb8e65f19bd274c
Jesús Sánchez Bertolín <jsbertolin@gmail.com>
2021-02-07 17:32:35 +0100

    add beer mapper

---
+++ src/main/java/guru/springframework/msscbrewery/mappers/BeerMapper.java
@@ -0,0 +1,11 @@
+package guru.springframework.msscbrewery.mappers;
+
+import guru.springframework.msscbrewery.domain.Beer;
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+    BeerDtoV2 beerToBeerDto(Beer beer);
+    Beer beerDtoToBeer(BeerDtoV2 beer);
+}
